da1fe5e8e42a57352d5f04803701852ae847cedb
BlondeVillain <bradley.shadowens@gmail.com>
2018-08-29 15:40:11 -0500

    first

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import {hashrouter} from 'react-router-dom';
+import router from './router';
+import {Provider} from 'react-redux';
+import store from 'src/store.js'
+ReactDOM.render(
 
-ReactDOM.render(<App />, document.getElementById('root'));
+<Provider store={store}>
+<HashRouter>
+<App />
+</HashRouter>
+</Provider>,
+
+document.getElementById('root'));
 registerServiceWorker();
 
